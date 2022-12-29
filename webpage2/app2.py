d901ee7fca3680206a55fd380b328dce90d92866
KSKA12 <112718352+KSKA12@users.noreply.github.com>
2022-12-29 16:54:39 +0200

    Update app2.py

--- webpage2/app2.py
+++ webpage2/app2.py
@@ -14,7 +14,7 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style",unsafe_allow_html=True)
         
-local_css("style/style.css")
+local_css("webpage2/style/style.css")
 
 lottie_coding = load_lottieurl ("https://assets5.lottiefiles.com/packages/lf20_ymEv4F.json")
 img_fac_logo = Image.open("images/biotech.png")
